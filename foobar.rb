184da76a36bab85ac8397c68fa3e9d7aff51b330
Jiahao Huang <jiahao1027study@berkeley.edu>
2015-09-13 23:50:32 -0700

    done

--- foobar.rb
+++ foobar.rb
@@ -1,3 +1,14 @@
 class Foobar
   # Q4 CODE HERE
+  def self.baz(a)
+  	a = a.uniq
+  	total = 0
+  	for i in a
+  		i = Integer(i)
+  		if i%2==0 && i < 10
+  			total = total + 2 + i
+  		end
+  	end
+  	return total
+  end
 end
